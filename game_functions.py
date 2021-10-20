d4c1fc680178b8a7f6f79dd18bb1eadbd12a6e03
alex <songkan@qq.com>
2021-10-20 21:12:28 +0800

    增加12.6.3左右移动

--- game_functions.py
+++ game_functions.py
@@ -1,6 +1,6 @@
 import sys 
 import pygame
-from pygame.constants import K_RIGHT 
+
 
 def check_events(ship):
     """响应按键和鼠标事件"""
@@ -10,14 +10,17 @@ def check_events(ship):
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = True
+            if event.key == pygame.K_LEFT:
+                ship.moving_left = True
+            
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = False
-
+            if event.key == pygame.K_LEFT:
+                ship.moving_left = False
 
 def update_screen(ai_settings, screen, ship):
     """更新屏幕上的图像，并切换到新屏幕"""
-    
     screen.fill(ai_settings.bg_color)
 
     # 每次循环时都要重绘屏幕
