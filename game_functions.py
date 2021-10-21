efc982fe31ff6d23f02458161d576c6fecfc5ad4
alex <songkan@qq.com>
2021-10-21 21:48:38 +0800

    12.6.6重构check_events()

--- game_functions.py
+++ game_functions.py
@@ -1,6 +1,18 @@
 import sys 
 import pygame
 
+def check_keydown_events(event, ship):
+    """相应按键"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    if event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    if event.key == pygame.K_LEFT:
+        ship.moving_left = False
 
 def check_events(ship):
     """响应按键和鼠标事件"""
@@ -8,17 +20,10 @@ def check_events(ship):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            if event.key == pygame.K_LEFT:
-                ship.moving_left = True
-            
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            if event.key == pygame.K_LEFT:
-                ship.moving_left = False
-
+            check_keyup_events(event, ship)
+            
 def update_screen(ai_settings, screen, ship):
     """更新屏幕上的图像，并切换到新屏幕"""
     screen.fill(ai_settings.bg_color)
