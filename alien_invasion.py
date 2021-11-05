46b93619169ca2ed388e5f0d385ae42b02b95687
alex <songkan@qq.com>
2021-11-05 20:41:58 +0800

    12.8.7 创建方法_update_bullets()

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,7 +2,6 @@ import pygame
 
 from settings import Settings
 from ship import Ship
-from bullet import Bullet
 import game_functions as gf
 
 
@@ -12,16 +11,21 @@ class AlienInvasion:
     def __init__(self):
         """初始化游戏并创建游戏资源"""
         pygame.init()
+
+        # 创建一个设置类
         self.settings = Settings()
 
+        # 创建屏幕，设置屏幕的宽度和高度 设置屏幕的标题
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
+        
+        # 创建飞船
         self.ship = Ship(self.settings, self)
+        
+        # 创建用于存储子弹的分组
         self.bullets = pygame.sprite.Group()
-
-
 
         # 设置背景色
         self.bg_color = (self.settings.bg_color)
@@ -29,10 +33,17 @@ class AlienInvasion:
     def run_game(self):
         """开始游戏的主循环"""
         while True:
+            
             # 监视鼠标和键盘事件
             gf._check_events(self)
-            self.ship.update()
-            self.bullets.update()
+            
+            # 更新飞船
+            self.ship.update()            
+            
+            # 更新子弹
+            gf._update_bullets(self)
+            
+            # 更新屏幕
             gf.update_screen(self)
 
 
--- game_functions.py
+++ game_functions.py
@@ -21,8 +21,9 @@ def _check_keyup_events(self, event):
 
 def _fire_bullet(self):
     """创建一颗子弹，并将其加入编组bullets中。"""
-    new_bullet = Bullet(self)
-    self.bullets.add(new_bullet)
+    if len(self.bullets) < self.settings.bullets_allowed:
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
 def _check_events(self):
     """响应按键和鼠标事件"""
@@ -33,14 +34,21 @@ def _check_events(self):
             _check_keydown_events(self, event)
         elif event.type == pygame.KEYUP:
             _check_keyup_events(self, event)
-            
+
+def _update_bullets(self):
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
 def update_screen(self):
     """更新屏幕上的图像，并切换到新屏幕"""
     self.screen.fill(self.settings.bg_color)
 
-    # 每次循环时都要重绘屏幕
+    # 每次循环时都要重绘飞船
     self.ship.blitme()
 
+    # 重绘组里面的子弹
     for bullet in self.bullets.sprites():
         bullet.draw_bullet()
 
